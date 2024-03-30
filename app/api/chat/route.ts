f35e9f78e12708e947be2c0ed2626a8c87f0428a
GA's Lab <125134406+GokulAnand14@users.noreply.github.com>
2024-03-30 04:54:27 +0000

    commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { HfInference } from '@huggingface/inference';
 import { HuggingFaceStream, StreamingTextResponse } from 'ai';
-import { experimental_buildOpenAssistantPrompt } from 'ai/prompts';
+import { experimental_buildLlama2Prompt } from 'ai/prompts';
+
 
 // Create a new HuggingFace Inference instance
 const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
@@ -13,15 +14,10 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const response = Hf.textGenerationStream({
-    model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
-    inputs: experimental_buildOpenAssistantPrompt(messages),
+    model: 'mistralai/Mistral-7B-Instruct-v0.2',
+    inputs: experimental_buildLlama2Prompt(messages),
     parameters: {
-      max_new_tokens: 200,
-      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
-      typical_p: 0.2,
-      repetition_penalty: 1,
-      truncate: 1000,
-      return_full_text: false,
+      max_new_tokens: 2000,
     },
   });
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,29 @@
 import { useChat } from 'ai/react';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialMessages: [
+      {
+        id: '',
+        content: `You are GAlliBOT, a helpful chatbot mady by GAllium who expains complex concepts in a 5year old level, use emojis`,
+        role: 'system'
+      },
+    ]
+  });
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+      {messages.length > 0
+            ? messages
+              .filter((m) => m.role !== 'system')
+              .map((m) => (
+                <div key={m.id} className="whitespace-pre-wrap p-5">
+                  {m.role === 'user' ? 'You: ' : 'teAIch: '}
+                  {m.content}
+                  <hr/>
+                </div>
+              ))
+        : null}
 
       <form onSubmit={handleSubmit}>
         <input
